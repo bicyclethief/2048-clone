5011359930ec4d54c69547ab9952e6db9efd6066
Vinh Bui <bicyclethief@gmail.com>
2015-04-22 07:48:35 -0400

    Add test for constructor with parameter of position string

--- spec/javascripts/game-spec.js
+++ spec/javascripts/game-spec.js
@@ -9,4 +9,9 @@ describe("Game", function(){
     var game = new Game();
     expect(game.toString()).toEqual("0 0 0 0\n0 0 0 0\n0 0 0 0\n0 0 0 0\n");
   });
+
+  it('should setup a board position', function() {
+    var game = new Game("0 0 0 1 2 0 0 0 0 2 0 0 0 0 0 4");
+    expect(game.toString()).toEqual("0 0 0 1\n2 0 0 0\n0 2 0 0\n0 0 0 4\n");
+  });
 });
