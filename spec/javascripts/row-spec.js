06dcb6360cccc34e295a89f4b75239b3e8383f64
Vinh Bui <bicyclethief@gmail.com>
2015-04-23 23:28:26 -0400

    Add simple collision tests for Row#moveLeft

--- spec/javascripts/row-spec.js
+++ spec/javascripts/row-spec.js
@@ -35,7 +35,24 @@ describe('Row#moveLeft', function() {
     expect(row.moveLeft()).toEqual([2, 0, 0, 0]);
   });
 
+  it('should handle a simple collision case', function() {
+    var row = new Row([2, 2, 0, 0]);
+    expect(row.moveLeft()).toEqual([4, 0, 0, 0]);
+  });
 
 
+  it('should handle a collision case when both tiles are not at the end', function() {
+    var row = new Row([0, 0, 2, 2]);
+    expect(row.moveLeft()).toEqual([4, 0, 0, 0]);
+  });
 
+  it('should handle a long distance collision', function() {
+    var row = new Row([2, 0, 0, 2]);
+    expect(row.moveLeft()).toEqual([4, 0, 0, 0]);
+  });
+
+  it('should double a tile only once', function() {
+    var row = new Row([2, 2, 2, 0]);
+    expect(row.moveLeft()).toEqual([4, 2, 0, 0]);
+  });
 });
