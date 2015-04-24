8b38498e79f6c007e493b848a418b212bf15d0ad
Vinh Bui <bicyclethief@gmail.com>
2015-04-23 23:04:38 -0400

    Add non collision tests for Row#moveLeft

--- spec/javascripts/row-spec.js
+++ spec/javascripts/row-spec.js
@@ -13,3 +13,29 @@ describe('Row#padArray', function() {
     expect(row.padArray(arrayToPad, 10, 0)).toEqual([1, 2, 3, 4, 0, 0, 0, 0, 0, 0]);
   });
 });
+
+describe('Row#moveLeft', function() {
+  it('should return same row when only non-zero digit is already furthest left', function() {
+    var row = new Row([2, 0, 0, 0]);
+    expect(row.moveLeft()).toEqual([2, 0, 0, 0]);
+  });
+
+  it('should return the second leftist non-zero digit shifted left', function() {
+    var row = new Row([0, 2, 0, 0]);
+    expect(row.moveLeft()).toEqual([2, 0, 0, 0]);
+  });
+
+  it('should return the third leftist non-zero digit shifted left', function() {
+    var row = new Row([0, 0, 2, 0]);
+    expect(row.moveLeft()).toEqual([2, 0, 0, 0]);
+  });
+
+  it('should return the rightest non-zero digit shifted left', function() {
+    var row = new Row([0, 0, 0, 2]);
+    expect(row.moveLeft()).toEqual([2, 0, 0, 0]);
+  });
+
+
+
+
+});
