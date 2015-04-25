6339b64948758f7f499f27a328117a986c2879dd
Vinh Bui <bicyclethief@gmail.com>
2015-04-25 01:05:31 -0400

    Add test for rotate90 method

--- spec/javascripts/array-helpers-spec.js
+++ spec/javascripts/array-helpers-spec.js
@@ -11,3 +11,11 @@ describe('padArray', function() {
     expect(padArray(arrayToPad, 10, 0)).toEqual([1, 2, 3, 4, 0, 0, 0, 0, 0, 0]);
   });
 });
+
+describe('rotate90', function() {
+  it('rotate an array 90 degrees clockwise', function() {
+    var game = new Game("1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0");
+    game.board = rotate90(game.board);
+    expect(game.toString()).toEqual("1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0");
+  });
+});
