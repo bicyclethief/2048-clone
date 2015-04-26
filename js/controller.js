699e084ce11f21bcf0b52c01e084fdcff34bb396
Vinh Bui <bicyclethief@gmail.com>
2015-04-25 22:23:19 -0400

    Check for win and unbind keys if so

--- js/controller.js
+++ js/controller.js
@@ -9,22 +9,44 @@ var Controller = function() {
   Mousetrap.bind('left', this._moveLeft.bind(this));
 };
 
+Controller.prototype.unbindAll = function() {
+  ['up', 'down', 'left', 'right'].forEach(function(key) {
+    Mousetrap.unbind(key);
+  });
+};
+
 Controller.prototype._moveUp = function() {
   this.game.move('up');
   console.log(this.game.toString());
+  if (this.game.isWon) {
+    alert("won!");
+    this.unbindAll();
+  }
 };
 
 Controller.prototype._moveDown = function() {
   this.game.move('down');
   console.log(this.game.toString());
+  if (this.game.isWon) {
+    alert("won!");
+    this.unbindAll();
+  }
 };
 
 Controller.prototype._moveRight = function() {
   this.game.move('right');
   console.log(this.game.toString());
+  if (this.game.isWon) {
+    alert("won!");
+    this.unbindAll();
+  }
 };
 
 Controller.prototype._moveLeft = function() {
   this.game.move('left');
   console.log(this.game.toString());
+  if (this.game.isWon) {
+    alert("won!");
+    this.unbindAll();
+  }
 };
