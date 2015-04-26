50d6209a545c55cd771ba11c3e7d0d65f5c3d836
Vinh Bui <bicyclethief@gmail.com>
2015-04-26 01:45:51 -0400

    Move Row class to its own file

--- js/cell.js
+++ js/cell.js
@@ -20,42 +20,4 @@ Cell.prototype.setDoubled = function() {
   hasDoubled = true;
 };
 
-var Row = function(row, game) {
-  this.row = row;
-  this.game = game;
-};
-
-Row.prototype.moveLeft = function() {
-  var leftOvers = removeZeros(this.row);
-  var cells = leftOvers.map(function(digit) {
-    return new Cell(digit);
-  });
-
-  for (var i = 1; i < cells.length; i++) {
-    previousCell = cells[i-1];
-    currentCell = cells[i];
-    if (!previousCell.hasDoubled && previousCell.equal(currentCell)) {
-      previousCell.doubleCell();
-      this.game.addScore(previousCell.value);
-      previousCell.setDoubled();
-      currentCell.emptyCell();
-      currentCell.setDoubled();
-    }
-  }
-
-  arrayToPad = this.cellsToArray(cells);
-  arrayToPad = removeZeros(arrayToPad);
-
-  return padArray(arrayToPad, 4, 0);
-};
-
-Row.prototype.cellsToArray = function(cells) {
-  return cells.map(function(cell) {
-    return cell.value;
-  });
-};
-
-// var row = new Row([2, 2, 0, 0]);
-// console.log(row.moveLeft());
-
 
